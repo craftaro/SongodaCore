19563b088ede6a29873b04956c3437711ce69317
jascotty2 <jascottytechie@gmail.com>
2019-10-27 13:11:38 -0500

    revert 312044c8 - issue was in implementation, not in core

--- Core/src/main/java/com/songoda/core/compatibility/CompatibleMaterial.java
+++ Core/src/main/java/com/songoda/core/compatibility/CompatibleMaterial.java
@@ -2138,14 +2138,12 @@ public enum CompatibleMaterial {
             case BEETROOTS:
             case CACTUS:
             case CARROTS:
-			case CARROT:
             case CHORUS_FLOWER:
                 // FROSTED_ICE is Ageable, but not a crop
             case KELP:
             case MELON_STEM:
             case NETHER_WART:
-			case POTATOES:
-			case POTATO:
+            case POTATOES:
             case PUMPKIN_STEM:
             case SUGAR_CANE:
             case WHEAT:
