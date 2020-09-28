0755fadc00de8c3ffbfa293c34bcaa4fd40f4347
Brianna <bri@songoda.com>
2020-09-28 11:19:46 -0500

    version 2.4.10

--- Core/src/main/java/com/songoda/core/SongodaCore.java
+++ Core/src/main/java/com/songoda/core/SongodaCore.java
@@ -54,7 +54,7 @@ public class SongodaCore {
     /**
      * This has been added as of Rev 6
      */
-    private final static String coreVersion = "2.4.9";
+    private final static String coreVersion = "2.4.10";
     
     /**
      * This is specific to the website api
