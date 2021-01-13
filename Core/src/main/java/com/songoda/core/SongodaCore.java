c7582778a5b44a4c06bdcd170a051fcdd20df1b3
Fernando Pettinelli <fernandopettinelli@l3tplay.com>
2021-01-13 20:57:38 -0300

    Version 2.4.34

--- Core/src/main/java/com/songoda/core/SongodaCore.java
+++ Core/src/main/java/com/songoda/core/SongodaCore.java
@@ -53,7 +53,7 @@ public class SongodaCore {
     /**
      * This has been added as of Rev 6
      */
-    private final static String coreVersion = "2.4.33";
+    private final static String coreVersion = "2.4.34";
 
     /**
      * This is specific to the website api
