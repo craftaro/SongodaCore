60ceac5eb6573d0e8f5bf13037ccb45bf4968b80
Christian Koop <contact@sprax2013.de>
2021-07-08 16:02:08 +0200

    Version 2.5.8

--- Core/src/main/java/com/songoda/core/SongodaCore.java
+++ Core/src/main/java/com/songoda/core/SongodaCore.java
@@ -53,7 +53,7 @@ public class SongodaCore {
     /**
      * This has been added as of Rev 6
      */
-    private final static String coreVersion = "2.5.7";
+    private final static String coreVersion = "2.5.8";
 
     /**
      * This is specific to the website api
