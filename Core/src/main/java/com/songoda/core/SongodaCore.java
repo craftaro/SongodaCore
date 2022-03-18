7ca814c74722780c090bac6193cc719683621d3c
Christian Koop <contact@sprax2013.de>
2022-03-18 14:47:22 +0100

    Release v2.6.12

--- Core/src/main/java/com/songoda/core/SongodaCore.java
+++ Core/src/main/java/com/songoda/core/SongodaCore.java
@@ -53,7 +53,7 @@ public class SongodaCore {
     /**
      * @since coreRevision 6
      */
-    private final static String coreVersion = "2.6.11";
+    private final static String coreVersion = "2.6.12";
 
     /**
      * This is specific to the website api
