8c0c281d9bfe1f6fef34cd2a9ef271cba8d0b7b3
Christian Koop <contact@sprax2013.de>
2022-03-18 14:29:06 +0100

    Deprecates Core's NBTItem implementations in favor of `item-nbt-api`

--- Core/src/main/java/com/songoda/core/SongodaPlugin.java
+++ Core/src/main/java/com/songoda/core/SongodaPlugin.java
@@ -4,6 +4,7 @@ import com.songoda.core.configuration.Config;
 import com.songoda.core.database.DataManagerAbstract;
 import com.songoda.core.locale.Locale;
 import com.songoda.core.utils.Metrics;
+import de.tr7zw.changeme.nbtapi.utils.MinecraftVersion;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
@@ -25,6 +26,12 @@ public abstract class SongodaPlugin extends JavaPlugin {
 
     protected ConsoleCommandSender console = Bukkit.getConsoleSender();
     private boolean emergencyStop = false;
+
+    static {
+        /* NBT-API */
+        MinecraftVersion.getLogger().setLevel(Level.WARNING);
+        MinecraftVersion.disableUpdateCheck();
+    }
 
     public abstract void onPluginLoad();
 
--- NMS/NMS-API/src/main/java/com/songoda/core/nms/nbt/NBTCore.java
+++ NMS/NMS-API/src/main/java/com/songoda/core/nms/nbt/NBTCore.java
@@ -4,8 +4,16 @@ import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
 
 public interface NBTCore {
+    /**
+     * @deprecated Use {@link de.tr7zw.nbtapi.NBTItem} instead.
+     */
+    @Deprecated
     NBTItem of(ItemStack item);
 
+    /**
+     * @deprecated Use {@link de.tr7zw.nbtapi.NBTItem} instead.
+     */
+    @Deprecated
     NBTItem newItem();
 
     NBTEntity of(Entity entity);
