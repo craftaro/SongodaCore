1ab918a32e7ba1c563e64d7e66dcc9ab3182f526
Fernando Pettinelli <fernandopettinellifreire@gmail.com>
2022-07-13 01:50:25 -0400

    Version 2.6.14-DEV

--- Core/src/main/java/com/songoda/core/SongodaCore.java
+++ Core/src/main/java/com/songoda/core/SongodaCore.java
@@ -53,7 +53,7 @@ public class SongodaCore {
     /**
      * @since coreRevision 6
      */
-    private final static String coreVersion = "2.6.13";
+    private final static String coreVersion = "2.6.14-DEV";
 
     /**
      * This is specific to the website api
