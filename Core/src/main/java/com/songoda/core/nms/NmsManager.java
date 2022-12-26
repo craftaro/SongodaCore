64865873f944c6327f150190856782d991eeec0b
Christian Koop <contact@sprax2013.de>
2022-12-26 13:44:25 +0100

    Adds NMS module for Spigot 1.19.3 (v1_19_R2) support

--- Core/src/main/java/com/songoda/core/nms/NmsManager.java
+++ Core/src/main/java/com/songoda/core/nms/NmsManager.java
@@ -141,6 +141,12 @@ public class NmsManager {
                     world = new com.songoda.core.nms.v1_19_R1v2.world.WorldCoreImpl();
                 }
                 break;
+            case "v1_19_R2":
+                player = new com.songoda.core.nms.v1_19_R2.entity.NMSPlayerImpl();
+                anvil = new com.songoda.core.nms.v1_19_R2.anvil.AnvilCore();
+                nbt = new com.songoda.core.nms.v1_19_R2.nbt.NBTCoreImpl();
+                world = new com.songoda.core.nms.v1_19_R2.world.WorldCoreImpl();
+                break;
             default:
                 Logger.getLogger(NmsManager.class.getName()).log(Level.SEVERE, "Failed to load NMS for this server version: version {0} not found", serverPackageVersion);
 
