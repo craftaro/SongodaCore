c29c4a5b0933a7adba189e455228a64fe83165f3
Christian Koop <contact@sprax2013.de>
2023-01-11 21:50:20 +0100

    Release v2.6.18-DEV

--- Core/src/main/java/com/songoda/core/SongodaCore.java
+++ Core/src/main/java/com/songoda/core/SongodaCore.java
@@ -55,7 +55,7 @@ public class SongodaCore {
     /**
      * @since coreRevision 6
      */
-    private final static String coreVersion = "2.6.17";
+    private final static String coreVersion = "2.6.18-DEV";
 
     /**
      * This is specific to the website api
