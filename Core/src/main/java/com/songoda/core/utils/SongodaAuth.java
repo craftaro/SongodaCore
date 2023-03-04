6145021ecbe87bb64150585b57806ac987dd34e0
Christian Koop <contact@sprax2013.de>
2023-03-04 12:47:53 +0100

    Remove unused imports in SongodaAuth and apply code formatting

--- Core/src/main/java/com/songoda/core/utils/SongodaAuth.java
+++ Core/src/main/java/com/songoda/core/utils/SongodaAuth.java
@@ -1,64 +1,52 @@
 package com.songoda.core.utils;
 
-import com.songoda.core.commands.AbstractCommand;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
 import org.bukkit.Bukkit;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.MessageDigest;
 import java.util.Properties;
 import java.util.UUID;
 
 public class SongodaAuth {
-
     public static boolean isAuthorized(boolean allowOffline) {
         String productId = "%%__PLUGIN__%%";
         try {
             Integer.parseInt(productId);
         } catch (NumberFormatException e) {
-            //Self compiled, return true
+            // Self compiled
             return true;
         }
+
         UUID uuid = getUUID();
         try {
             URL url = new URL("https://marketplace.songoda.com/api/v2/products/license/validate");
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("POST");
             con.setRequestProperty("Content-Type", "application/json");
             con.setRequestProperty("Accept", "application/json");
             con.setDoOutput(true);
             String jsonInputString = "{\"product_id\":" + productId + ",\"license\":\"" + uuid + "\",\"user_id\":\"%%__USER__%%\"}";
-            try(OutputStream os = con.getOutputStream()) {
+            try (OutputStream os = con.getOutputStream()) {
                 byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
                 os.write(input, 0, input.length);
             }
 
-            try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
                 StringBuilder response = new StringBuilder();
-                String responseLine = null;
+                String responseLine;
                 while ((responseLine = br.readLine()) != null) {
                     response.append(responseLine.trim());
                 }
+
                 JSONObject jsonObject = (JSONObject) new JSONParser().parse(response.toString());
                 if (jsonObject.get("error") != null) {
                     //Got an error, return false and print error
@@ -74,7 +62,7 @@ public class SongodaAuth {
     }
 
     public static UUID getUUID() {
-        File serverProperties = new File(new File("."),"server.properties");
+        File serverProperties = new File(new File("."), "server.properties");
         Properties prop = new Properties();
         try {
             prop.load(new FileReader(serverProperties));
@@ -95,18 +83,19 @@ public class SongodaAuth {
     public static String getIP() {
         try {
             URL url = new URL("https://marketplace.songoda.com/api/v2/products/license/ip");
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("Accept", "application/json");
             con.setDoOutput(true);
-            try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
                 StringBuilder response = new StringBuilder();
-                String responseLine = null;
+                String responseLine;
                 while ((responseLine = br.readLine()) != null) {
                     response.append(responseLine.trim());
                 }
-                JSONParser parser = new JSONParser();
-                JSONObject jsonObject = (JSONObject) parser.parse(response.toString());
+
+                JSONObject jsonObject = (JSONObject) new JSONParser().parse(response.toString());
                 return jsonObject.get("ip").toString();
             }
         } catch (Exception ex) {
