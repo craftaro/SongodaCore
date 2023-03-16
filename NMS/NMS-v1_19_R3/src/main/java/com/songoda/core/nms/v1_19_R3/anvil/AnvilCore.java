52f6c42266b12a876830cd581b6d2e4d2f436ba7
Christian Koop <contact@sprax2013.de>
2023-03-16 09:44:22 +0100

    Adds NMS Module v1_19_R3 for Spigot 1.19.4 compatibility

---
+++ NMS/NMS-v1_19_R3/src/main/java/com/songoda/core/nms/v1_19_R3/anvil/AnvilCore.java
@@ -0,0 +1,21 @@
+package com.songoda.core.nms.v1_19_R3.anvil;
+
+import com.songoda.core.nms.anvil.CustomAnvil;
+import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.craftbukkit.v1_19_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.InventoryHolder;
+
+public class AnvilCore implements com.songoda.core.nms.anvil.AnvilCore {
+    @Override
+    public CustomAnvil createAnvil(Player player) {
+        ServerPlayer p = ((CraftPlayer) player).getHandle();
+        return new AnvilView(p.nextContainerCounter(), p, null);
+    }
+
+    @Override
+    public CustomAnvil createAnvil(Player player, InventoryHolder holder) {
+        ServerPlayer p = ((CraftPlayer) player).getHandle();
+        return new AnvilView(p.nextContainerCounter(), p, holder);
+    }
+}
