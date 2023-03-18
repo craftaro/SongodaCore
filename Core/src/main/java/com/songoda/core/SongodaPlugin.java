422032a2af507fd43fdee6a3f378484148ce424a
ceze88 <dev.ceze@gmail.com>
2023-03-18 23:29:08 +0100

    Add Database converter

--- Core/src/main/java/com/songoda/core/SongodaPlugin.java
+++ Core/src/main/java/com/songoda/core/SongodaPlugin.java
@@ -6,6 +6,9 @@ import co.aikar.taskchain.TaskChainFactory;
 import com.songoda.core.actions.ActionManager;
 import com.songoda.core.builtin.SongodaCoreCommand;
 import com.songoda.core.configuration.Config;
+import com.songoda.core.database.DataManager;
+import com.songoda.core.database.DataMigration;
+import com.songoda.core.database.DatabaseType;
 import com.songoda.core.placeholder.IPlaceholderResolver;
 import com.songoda.core.placeholder.NoPluginResolver;
 import com.songoda.core.placeholder.PlaceholderAPIResolver;
@@ -19,6 +22,9 @@ import revxrsal.commands.bukkit.BukkitCommandHandler;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 
 public abstract class SongodaPlugin extends JavaPlugin {
@@ -37,6 +43,7 @@ public abstract class SongodaPlugin extends JavaPlugin {
     private IPlaceholderResolver placeholderResolver;
     private ActionManager actionManager;
     private TaskChainFactory taskChainFactory;
+    private DataManager dataManager;
 
     public abstract void onPluginEnable();
     public abstract void onPluginDisable();
@@ -60,6 +67,54 @@ public abstract class SongodaPlugin extends JavaPlugin {
         }
 
         onPluginEnable();
+    }
+    /**
+     * Get the DataManager for this plugin.
+     * Note: Make sure to call initDatabase() in onPluginEnable() before using this.
+     * @return DataManager for this plugin.
+     */
+    public DataManager getDataManager() {
+        return dataManager;
+    }
+
+    /**
+     * Initialize the DataManager for this plugin and convert from SQLite to H2 if needed.
+     */
+    protected void initDatabase() {
+        this.dataManager = new DataManager(this, Collections.emptyList());
+        if (dataManager.getDatabaseConnector().isInitialized()) {
+            //Check if the type is SQLite
+            if (dataManager.getDatabaseConnector().getType() == DatabaseType.SQLITE) {
+                //Let's convert it to H2
+                DataManager newDataManager = DataMigration.convert(this, DatabaseType.H2);
+                if (newDataManager != null && newDataManager.getDatabaseConnector().isInitialized()) {
+                    //Set the new data manager
+                    setDataManager(newDataManager);
+                }
+            }
+        }
+    }
+
+    /**
+     * Initialize the DataManager for this plugin.
+     * @param migrations List of migrations to run.
+     */
+    protected void initDatabase(List<DataMigration> migrations) {
+        this.dataManager = new DataManager(this, migrations);
+    }
+
+    /**
+     * Set the DataManager for this plugin.
+     * Used for converting from one database to another.
+     */
+    public void setDataManager(DataManager dataManager) {
+        if (dataManager == null) throw new IllegalArgumentException("DataManager cannot be null!");
+        if (this.dataManager == dataManager) return;
+        //Make sure to shut down the old data manager.
+        if (this.dataManager != null) {
+            dataManager.shutdown();
+        }
+        this.dataManager = dataManager;
     }
 
     /**
--- Core/src/main/java/com/songoda/core/builtin/SongodaCoreCommand.java
+++ Core/src/main/java/com/songoda/core/builtin/SongodaCoreCommand.java
@@ -1,10 +1,17 @@
 package com.songoda.core.builtin;
 
 import com.songoda.core.SongodaCore;
+import com.songoda.core.SongodaPlugin;
 import com.songoda.core.compatibility.ClassMapping;
 import com.songoda.core.compatibility.ServerProject;
 import com.songoda.core.compatibility.ServerVersion;
+import com.songoda.core.database.DataManager;
+import com.songoda.core.database.DataMigration;
+import com.songoda.core.database.DatabaseType;
 import com.songoda.core.plugins.PluginInfo;
+import com.songoda.core.utils.ColorUtils;
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import revxrsal.commands.annotation.Command;
@@ -72,5 +79,37 @@ public class SongodaCoreCommand {
                 ex.printStackTrace();
             }
         }
+    }
+
+    @Subcommand("convertdb")
+    public void onConvertDB(CommandSender sender, String plugin, String to) {
+        DatabaseType type;
+        try {
+            type = DatabaseType.valueOf(to.toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            sender.sendMessage("Invalid database type.");
+            return;
+        }
+        SongodaPlugin songodaPlugin = (SongodaPlugin) SongodaCore.getInstance().getRegisteredPlugins().stream()
+                .filter(info -> info.getJavaPlugin().getName().equalsIgnoreCase(plugin))
+                .findFirst().map(PluginInfo::getJavaPlugin).orElse(null);
+        if (songodaPlugin == null) {
+            sender.sendMessage("Invalid plugin.");
+            return;
+        }
+        if (songodaPlugin.getDataManager().getDatabaseConnector().getType() == type) {
+            sender.sendMessage("Database is already " + type.name() + ".");
+            return;
+        }
+
+        Thread t = new Thread(() -> {
+            DataManager newManager = DataMigration.convert(songodaPlugin, type);
+            if (newManager == null || !newManager.getDatabaseConnector().isInitialized()) {
+                sender.sendMessage("Failed to convert database.");
+                return;
+            }
+            songodaPlugin.setDataManager(newManager);
+            sender.sendMessage("Converted database to " + type.name() + ".");
+        });
     }
 }
