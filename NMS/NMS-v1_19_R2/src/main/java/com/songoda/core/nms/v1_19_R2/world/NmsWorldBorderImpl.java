2077f0fb684c1ca38f9b2ba1a9940aaee047e61a
Christian Koop <contact@sprax2013.de>
2023-05-18 11:27:01 +0200

    Adds missing NMS classes for v1_19_R2 and v1_19_R3 after merge
    Introduced in 2c1f8bbdf847c808a44e41f3a301cc7dd6788042

---
+++ NMS/NMS-v1_19_R2/src/main/java/com/songoda/core/nms/v1_19_R2/world/NmsWorldBorderImpl.java
@@ -0,0 +1,34 @@
+package com.songoda.core.nms.v1_19_R2.world;
+
+import com.songoda.core.nms.world.NmsWorldBorder;
+import net.minecraft.network.protocol.game.ClientboundInitializeBorderPacket;
+import net.minecraft.world.level.border.WorldBorder;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_19_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_19_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
+
+public class NmsWorldBorderImpl implements NmsWorldBorder {
+    @Override
+    public void send(Player player, BorderColor color, double size, Location center) {
+        Objects.requireNonNull(center.getWorld());
+
+        WorldBorder worldBorder = new WorldBorder();
+        worldBorder.world = ((CraftWorld) center.getWorld()).getHandle();
+
+        worldBorder.setCenter(center.getX(), center.getZ());
+        worldBorder.setSize(size);
+        worldBorder.setWarningTime(0);
+        worldBorder.setWarningBlocks(0);
+
+        if (color == BorderColor.GREEN) {
+            worldBorder.lerpSizeBetween(size - 0.1D, size, Long.MAX_VALUE);
+        } else if (color == BorderColor.RED) {
+            worldBorder.lerpSizeBetween(size, size - 1.0D, Long.MAX_VALUE);
+        }
+
+        ((CraftPlayer) player).getHandle().connection.send(new ClientboundInitializeBorderPacket(worldBorder));
+    }
+}
