b00660942ba04b206e3eaab7b452fe67a9e647a6
Christian Koop <contact@sprax2013.de>
2023-06-18 00:22:03 +0200

    Adds some more server versions to Compatibility ServerVersion class

--- Compatibility/src/main/java/com/craftaro/core/compatibility/ServerVersion.java
+++ Compatibility/src/main/java/com/craftaro/core/compatibility/ServerVersion.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.bukkit.Bukkit;
 
 public enum ServerVersion {
-    UNKNOWN, V1_7, V1_8, V1_9, V1_10, V1_11, V1_12, V1_13, V1_14, V1_15, V1_16, V1_17, V1_18, V1_19, V1_20, V1_21;
+    UNKNOWN, V1_7, V1_8, V1_9, V1_10, V1_11, V1_12, V1_13, V1_14, V1_15, V1_16, V1_17, V1_18, V1_19, V1_20, V1_21, V1_22, V1_23;
 
     private static final String serverPackageVersion;
     private static final String serverReleaseVersion;
