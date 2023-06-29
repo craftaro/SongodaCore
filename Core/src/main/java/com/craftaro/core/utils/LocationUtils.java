9ac1da984d618c9ea2197ae6bb34df99dbe8208f
Christian Koop <contact@sprax2013.de>
2023-06-29 09:37:42 +0200

    Renamed method argument `l` to `location` in `LocationUtils#isInArea`

--- Core/src/main/java/com/craftaro/core/utils/LocationUtils.java
+++ Core/src/main/java/com/craftaro/core/utils/LocationUtils.java
@@ -12,7 +12,7 @@ public class LocationUtils {
                 location1.getBlockZ() == location2.getBlockZ();
     }
 
-    public static boolean isInArea(Location l, Location pos1, Location pos2) {
+    public static boolean isInArea(Location location, Location pos1, Location pos2) {
         double x1 = Math.min(pos1.getX(), pos2.getX());
         double y1 = Math.min(pos1.getY(), pos2.getY());
         double z1 = Math.min(pos1.getZ(), pos2.getZ());
@@ -21,8 +21,8 @@ public class LocationUtils {
         double y2 = Math.max(pos1.getY(), pos2.getY());
         double z2 = Math.max(pos1.getZ(), pos2.getZ());
 
-        return l.getX() >= x1 && l.getX() <= x2 &&
-                l.getY() >= y1 && l.getY() <= y2 &&
-                l.getZ() >= z1 && l.getZ() <= z2;
+        return location.getX() >= x1 && location.getX() <= x2 &&
+                location.getY() >= y1 && location.getY() <= y2 &&
+                location.getZ() >= z1 && location.getZ() <= z2;
     }
 }
